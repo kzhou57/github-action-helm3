da177e7c9a15b15f3fa8c09ba68f00068af27690
Kevin Zhou <developer.kevin@gmail.com>
2025-10-29 12:02:07 -0700

    Debug

--- main.js
+++ main.js
@@ -79,9 +79,11 @@ async function main() {
         '}\n' +
         ' \n' +
         'curl -s -o ' + dockerKubeConfigDir + ' "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$(dpkg --print-architecture)/kubectl" 2>&1\n' +
-        'curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > /dev/null 2>&1\n' +
+        'curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 \n' +
         'chmod 700 get_helm.sh > /dev/null 2>&1\n' +
-        'DESIRED_VERSION="' + (process.env.INPUT_HELM_VERSION || '') + '" HELM_INSTALL_DIR=' + dockerKubeConfigDir + ' ./get_helm.sh > /dev/null 2>&1\n' +
+        'DESIRED_VERSION="' + (process.env.INPUT_HELM_VERSION || '') + '" HELM_INSTALL_DIR=' + dockerKubeConfigDir + ' ./get_helm.sh \n' +
+        'cat ./get_helm.sh \n' +
+        'helm version \n' +
         'rm ./get_helm.sh > /dev/null 2>&1\n' +
         ' \n' +
         process.env.INPUT_EXEC
